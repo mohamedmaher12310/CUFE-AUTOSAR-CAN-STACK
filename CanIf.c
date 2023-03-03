627ea1e787ecd273e813ea2f161b33e03185c5ee
mahoora <mohamedmaher12310@gmail.com>
2023-03-03 13:44:31 +0200

    integration

--- CanIf.c
+++ CanIf.c
@@ -12,28 +12,3 @@
 /*Include the module header file*/
 #include "CanIf.h"
 
-/************************************************************************************
- * Service Name: CanIf_RxIndication
- * Service ID[hex]: 0x14
- * Sync/Async: Synchronous
- * Reentrancy:  Reentrant
- * Parameters (in):
- *          Mailbox: Identifies the HRH and its corresponding CAN Controller
- *          PduInfoPtr :Pointer to the received L-PDUParameters (inout): None
- * Parameters (out): None
- * Return value:     None
- * Description: This service indicates a successful reception of a received CAN Rx LPDU
- *              to the CanIf after passing all filters and validation checks.
- ************************************************************************************/
-
-void CanIf_RxIndication(const Can_HwType* Mailbox,const PduInfoType* PduInfoPtr)
-{
-
-    CanIf_PduModeType CanIf_RxIndication_Channel_Mode = CANIF_OFFLINE ;
-
-
-    uint8 channel_index = CanIf_GetHrhChannel(Mailbox ->Hoh);
-
-
-
-}
