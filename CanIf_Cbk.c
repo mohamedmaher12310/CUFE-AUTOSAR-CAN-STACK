4d59c5e6a8e1469e60d5c18135348ad772c88652
Omar Khedr <omaradelkhedr@gmail.com>
2023-03-05 23:28:18 +0200

    Rx_Indication (mn3'er nadafa)

--- CanIf_Cbk.c
+++ CanIf_Cbk.c
@@ -9,28 +9,54 @@
  * Author: CUFE 2023 Team
  ******************************************************************************/
 #include "CanIf_Cbk.h"
-#include "CanIf_Types.h"
 
-/******************************************************************************
- *Service name: CanIf_RxIndication
- *Syntax: void CanIf_RxIndication(const Can_HwType* Mailbox,const PduInfoType* PduInfoPtr)
- *Service ID[hex]: 0x14
- *Sync/Async: Synchronous
- *Reentrancy: Reentrant
- *Parameters (in):  - Mailbox Identifies the HRH and its corresponding CAN Controller
-                    -PduInfoPtr Pointer to the received L-PDU
- *Parameters (inout): None
- *Parameters (out): None
- *Return value: None
- *Description: This service indicates a successful reception of a received CAN Rx LPDU to the CanIf after passing all filters and validation checks
- ******************************************************************************/
+/*CanIf Recieve Buffer (ReadAPI)*/
+#if (STD_ON == CanIfPublicReadRxPduDataApi)
+uint64 CanIf_RxBuffer[CanIfMaxRxPduCfg]={ZERO};
+#endif /*CanIfPublicReadRxPduDataApi*/
+
+/*Rx Notification Flags*/
+#if (STD_ON == CanIfPublicReadRxPduNotifyStatusApi)
+CanIf_NotifStatusType CanIf_RxNotificationFlag[CanIfMaxRxPduCfg]={ZERO};
+#endif /*CanIfPublicReadRxPduNotifyStatusApi*/
+
+/************************************************************************************
+ * Service Name: CanIf_RxIndication
+ * Service ID[hex]: 0x14
+ * Sync/Async: Synchronous
+ * Reentrancy: Reentrant
+ * Parameters (in): Mailbox - Identifies the HRH and its corresponding CAN Controller
+ *                  PduInfoPtr - Pointer to the received L-PDU
+ * Parameters (inout): None
+ * Parameters (out): None
+ * Return value: None
+ * Description: Function to indicate a successful reception of a received CAN Rx L-PDU
+ *              to the CanIf after passing all filters and validation checks.
+ ************************************************************************************/
 void CanIf_RxIndication(const Can_HwType* Mailbox, const PduInfoType * PduInfoPtr)
 {
-
-    /* If parameter Mailbox->Hoh of CanIf_RxIndication() has an invalid value, CanIf shall report development error code*/
+    /*needed pointers*/
+    CanIfRxPduCfg* RxPDU = NULL_PTR;
+    CanIfHrhRangeCfg* RxPDU_Range = NULL_PTR;
+    CanIfHrhCfg* HRH_index_Ptr = NULL_PTR;
+    /*needed variables*/
+    uint8 RxPDU_index ;
+    uint8 HRH_index ;
+    uint8 PDU_PASS=ZERO;
+    /* NES2AL FL ENUM wel comparison ely fe page 41 [SWS_CANIF_00877] */
+    boolean Extended=ZERO;
+    Can_IdType Id_Type = ( (Mailbox->CanId && (0xC0000000)) >> 29 ) ;
+    if (Id_Type == STANDARD_CAN_Rx)
+    {
+        Extended = ZERO;
+    }
+    else if (Id_Type == EXTENDED_CAN_Rx)
+    {
+        Extended = ONE;
+    }
 #if(STD_ON == CanIfDevErrorDetect)
-
-    if(Mailbox->Hoh != RECIEVE)
+    /* If parameter Mailbox->Hoh of CanIf_RxIndication() has an invalid value, CanIf shall report development error code*/
+    if(Mailbox->Hoh > CAN_HOH_NUMBER)
     {
         Det_ReportError(CANIF_MODULE_ID, CANIF_INSTANCE_ID, CanIf_RxIndication_RXID_SID, CANIF_E_PARAM_HOH);
     }
@@ -38,8 +64,10 @@ void CanIf_RxIndication(const Can_HwType* Mailbox, const PduInfoType * PduInfoPt
     {
         /*MISRA : do nothing*/
     }
-
-    /* If parameter Mailbox->CanId of CanIf_RxIndication() has an invalid value, CanIf shall report development error code CANIF_E_PARAM_CANID to the Det_ReportError service of the DET */
+    /*If parameter Mailbox->CanId of CanIf_RxIndication() has an invalid value,
+     *CanIf shall report development error code CANIF_E_PARAM_CANID to the Det_ReportError
+     *service of the DET
+     */
     if ( (Mailbox->CanId > CANNIF_STANDARD_MAX) || (Mailbox->CanId >CANNIF_EXTENDED_MAX) )
     {
         Det_ReportError(CANIF_MODULE_ID, CANIF_INSTANCE_ID, CanIf_RxIndication_RXID_SID, CANIF_E_PARAM_CANID);
@@ -48,51 +76,100 @@ void CanIf_RxIndication(const Can_HwType* Mailbox, const PduInfoType * PduInfoPt
     {
         /*MISRA : do nothing*/
     }
-
-
-    /*If parameter PduInfoPtr or Mailbox has an invalid value, CanIf shall report development error code CANIF_E_PARAM_POINTER to the Det_ReportError service of the DET module*/
+    /*If parameter PduInfoPtr or Mailbox has an invalid value,
+     *CanIf shall report development error code CANIF_E_PARAM_POINTER to
+     *the Det_ReportError service of the DET module
+     */
     if( ( NULL_PTR == Mailbox) || ( NULL_PTR== PduInfoPtr)  )
     {
 
         Det_ReportError(CANIF_MODULE_ID, CANIF_INSTANCE_ID, CanIf_RxIndication_RXID_SID, CANIF_E_PARAM_POINTER);
     }
     else
-    {
-        /*MISRA : do nothing*/
-    }
-
-
 #endif
-    const CanIfRxPduCfg* RxPDU = NULL_PTR;
-    uint8 RxPDU_index ;
-    RxPDU=&CanIf_Configuration.CanIfInitCfg.CanIfRxPduCfg;
-
-    RxPDU_index = RxPDU->CanIfRxPduId;
-
-    //    uint16 iter;
-    uint8 PDU_PASS=8;
-    if(BASIC == (RxPDU->CanIfRxPduHrhIdRef->CanIfHrhIdSymRef->CanHandleType) )
     {
-        /*check of SW filter enable*/
-        if(TRUE == (RxPDU->CanIfRxPduHrhIdRef->CanIfHrhSoftwareFilter))
+
+        RxPDU=&(CanIf_Configuration.CanIfInitCfg.CanIfRxPduCfg);
+        RxPDU_index = RxPDU->CanIfRxPduId;
+        HRH_index_Ptr = &CanIf_Configuration.CanIfInitCfg.CanIfRxPduCfg[RxPDU_index].CanIfRxPduHrhIdRef;
+        HRH_index= (HRH_index_Ptr->CanIfHrhIdSymRef->CanObjectId);
+        RxPDU_Range = &CanIf_Configuration.CanIfInitCfg.CanIfInitHohCfg[Can_DRIVERS_NUMBER].CanIfHrhCfg[HRH_index].CanIfHrhRangeCfg;
+
+        /***************************************before all of this check the channel mode of the  PDU ************************/
+        //        if( (CANIF_TX_OFFLINE == RxPDU->CanIf_PduModeType) || (CANIF_ONLINE == RxPDU->CanIf_PduModeType) )
+        //        {
+
+        if(BASIC == (RxPDU->CanIfRxPduHrhIdRef->CanIfHrhIdSymRef->CanHandleType) )
         {
-            if( ( (Mailbox->CanId) & (RxPDU->CanIfRxPduCanIdMask) ) == (RxPDU->CanIfRxPduCanIdMask)&(RxPDU->CanIfRxPduCanId) )
+
+
+            /*check of SW filter enable*/
+            if(TRUE == (RxPDU->CanIfRxPduHrhIdRef->CanIfHrhSoftwareFilter))
             {
-                PDU_PASS=1;
+                if( ( ((Mailbox->CanId) &(RxPDU->CanIfRxPduCanIdMask)) >= (RxPDU_Range->CanIfHrhRangeRxPduLowerCanId) ) && ( ((Mailbox->CanId)&(RxPDU->CanIfRxPduCanIdMask)) <= (RxPDU_Range->CanIfHrhRangeRxPduUpperCanId) )  )
+                {
+                    PDU_PASS=1;
+                }
+                else
+                {
+                    PDU_PASS=0;
+                }
             }
             else
             {
-                PDU_PASS=0;
+
             }
-        }
-        else
-        {
-
-        }
-        /*after passing sw filter test*/
-        if( 1 == PDU_PASS ){
+            /*after passing sw filter test*/
+            if( 1 == PDU_PASS ){
 #if (CanIfPrivateDataLengthCheck == STD_ON)
+                if((PduInfoPtr->SduLength) > (RxPDU->CanIfRxPduDataLength))
+                {
+                    Det_ReportError(CANIF_MODULE_ID, CANIF_INSTANCE_ID, CanIf_RxIndication_RXID_SID, CANIF_E_INVALID_DATA_LENGTH);
 
+                }
+#endif
+                else/*length check is ok call the upper layer*/
+                {
+                    /*CanIf Recieve Buffer (ReadAPI)*/
+#if (STD_ON == CanIfPublicReadRxPduDataApi)
+                    //Nsta2bel el message we n5znha fl CanIf buffer CanIf_RxBuffer
+
+#endif /*CanIfPublicReadRxPduDataApi*/
+
+                    /*Rx Notification Flags*/
+#if (STD_ON == CanIfPublicReadRxPduNotifyStatusApi)
+                    CanIf_RxNotificationFlag[RxPDU_index]=CANIF_TX_RX_NOTIFICATION;
+#endif /*CanIfPublicReadRxPduNotifyStatusApi*/
+
+                    switch(RxPDU->CanIfRxPduUserRxIndicationName)
+                    {
+                    case PDUR :
+                    {
+                        PduInfoType RxPduPDUR;
+                        RxPduPDUR.SduLength = PduInfoPtr->SduLength;
+                        RxPduPDUR.SduDataPtr = PduInfoPtr->SduDataPtr;
+                        RxPduPDUR.MetaDataPtr = PduInfoPtr->MetaDataPtr;
+                        //PDUR_RxIndication(RxPDU_index,&RxPduPDUR);
+
+                        break;
+                    }
+                    default:
+                        break;
+
+
+                    }
+
+                }
+            }
+            else/*PDU_PASS=0*/
+            {
+
+            }
+
+        }
+        else if( ( (Mailbox->CanId) & (RxPDU->CanIfRxPduCanIdMask) ) == (RxPDU->CanIfRxPduCanIdMask)&(RxPDU->CanIfRxPduCanId) )
+        {               /*FULL CAN*/
+#if (CanIfPrivateDataLengthCheck == STD_ON)
             if((PduInfoPtr->SduLength) > (RxPDU->CanIfRxPduDataLength))
             {
                 Det_ReportError(CANIF_MODULE_ID, CANIF_INSTANCE_ID, CanIf_RxIndication_RXID_SID, CANIF_E_INVALID_DATA_LENGTH);
@@ -101,6 +178,17 @@ void CanIf_RxIndication(const Can_HwType* Mailbox, const PduInfoType * PduInfoPt
 #endif
             else/*length check is ok call the upper layer*/
             {
+
+                /*CanIf Recieve Buffer (ReadAPI)*/
+#if (STD_ON == CanIfPublicReadRxPduDataApi)
+                //Nsta2bel el message we n5znha fl CanIf buffer CanIf_RxBuffer
+
+#endif /*CanIfPublicReadRxPduDataApi*/
+
+                /*Rx Notification Flags*/
+#if (STD_ON == CanIfPublicReadRxPduNotifyStatusApi)
+                CanIf_RxNotificationFlag[RxPDU_index]=CANIF_TX_RX_NOTIFICATION;
+#endif /*CanIfPublicReadRxPduNotifyStatusApi*/
 
 
                 switch(RxPDU->CanIfRxPduUserRxIndicationName)
@@ -111,7 +199,7 @@ void CanIf_RxIndication(const Can_HwType* Mailbox, const PduInfoType * PduInfoPt
                     RxPduPDUR.SduLength = PduInfoPtr->SduLength;
                     RxPduPDUR.SduDataPtr = PduInfoPtr->SduDataPtr;
                     RxPduPDUR.MetaDataPtr = PduInfoPtr->MetaDataPtr;
-                    PDUR_RxIndication(RxPDU_index,&RxPduPDUR);
+                    //PDUR_RxIndication(RxPDU_index,&RxPduPDUR);
 
                     break;
                 }
@@ -122,45 +210,12 @@ void CanIf_RxIndication(const Can_HwType* Mailbox, const PduInfoType * PduInfoPt
                 }
 
             }
-        }
-        else/*PDU_PASS=0*/
-        {
 
         }
-
-    }
-    else{/*FULL CAN*/
-#if (CanIfPrivateDataLengthCheck == STD_ON)
-
-        if((PduInfoPtr->SduLength) > (RxPDU->CanIfRxPduDataLength))
+        //}
+        else
         {
-            Det_ReportError(CANIF_MODULE_ID, CANIF_INSTANCE_ID, CanIf_RxIndication_RXID_SID, CANIF_E_INVALID_DATA_LENGTH);
-
-        }
-#endif
-        else/*length check is ok call the upper layer*/
-        {
-
-
-            switch(RxPDU->CanIfRxPduUserRxIndicationName)
-            {
-            case PDUR :
-            {
-                PduInfoType RxPduPDUR;
-                RxPduPDUR.SduLength = PduInfoPtr->SduLength;
-                RxPduPDUR.SduDataPtr = PduInfoPtr->SduDataPtr;
-                RxPduPDUR.MetaDataPtr = PduInfoPtr->MetaDataPtr;
-                PDUR_RxIndication(RxPDU_index,&RxPduPDUR);
-
-                break;
-            }
-            default:
-                break;
-
-
-            }
 
         }
     }
-
 }
--- Can_IRQ.c
+++ Can_IRQ.c
@@ -22,7 +22,11 @@ volatile uint32 Transmit_Count =0; /* variable that increments when a message is
 uint8 HOH;
 void CAN0_Handler(void)
 {
-    //MSG_Number_INT =0;  /* clearing the message number variable for re-use */
+    /*CanIf_RxIndication Arguments*/
+    Can_HwType CanIfRx_Mailbox;
+    PduInfoType CanIfRx_PduInfoPtr;
+    Can_IdType CanIfRx_Id;
+
     MSG_Object_INT_Flag=0;  /* clearing the message object flag for re-use */
     uint32 status = REG_VAL(CAN0_BASE,CAN_INT_OFFSET);  /* reading the CAN_INT register to detect the interrupt cause */
     if (CAN_INT_Status_Interrupt == status )  /* if interrupt is a Status interrupt */
@@ -79,7 +83,39 @@ void CAN0_Handler(void)
         }
         else if(Can_Configuration.CanConfigSet.CanHardwareObject[HOH].CanObjectType==RECIEVE) /* interrupt caused by receive message object */
         {
-            /*CanIf_Rxindication()*/
+            /*Lock Mailbox until Reading of Pdu from Users*/
+            Object_Check[CAN0_CONTROLLER_ID][HOH][Mailbox_Index].Check = Unconfirmed;
+            /*Setting argument values of CanIf_RxIndication*/
+            REG_VAL(CAN0_BASE,CAN_IF1CRQ_OFFSET) =  status & (SIX_BIT_MASK);
+            /*Check if the ID is standard*/
+            if( BIT_IS_CLEAR( REG_VAL(CAN0_BASE,CAN_IF1ARB2_OFFSET),XTD_BIT) )
+            {
+                CanIfRx_Id = ( REG_VAL(CAN0_BASE,CAN_IF1ARB2_OFFSET) & CANIF1ARB2_ID_MASK ) >> 2;
+            }
+            else
+            {
+                /*
+                 *   ID is Extended
+                 */
+                CanIfRx_Id = (( REG_VAL(CAN0_BASE,CAN_IF1ARB2_OFFSET) & CANIF1ARB2_ID_MASK ) <<16) | REG_VAL(CAN0_BASE,CAN_IF1ARB1_OFFSET) ;
+            }
+            CanIfRx_Mailbox.CanId = CanIfRx_Id;
+            /*Controller ID with respect to CanDrv or CanIf ?!*/
+            CanIfRx_Mailbox.ControllerId = CAN0_CONTROLLER_ID;
+            CanIfRx_Mailbox.Hoh = Can_Configuration.CanConfigSet.CanHardwareObject[HOH].CanObjectId;
+
+            /*Is this Right or Wrong?! DATA NORMALIZATION*/
+
+            CanIfRx_PduInfoPtr.SduLength = (REG_VAL(CAN0_BASE,CAN_IF1MCTL_OFFSET) & CANIF1MCTL_DLC_MASK);
+            CanIfRx_PduInfoPtr.SduDataPtr = &(REG_VAL(CAN0_BASE,CAN_IF1DA1_OFFSET));
+
+            /*Call User (CanIf) to indicate recieved message*/
+            CanIf_RxIndication(&CanIfRx_Mailbox,&CanIfRx_PduInfoPtr);
+            /*
+             * The hardware object will be immediately released
+             * after CanIf_RxIndication() of CanIf returns to avoid loss of data.
+             */
+            Object_Check[CAN0_CONTROLLER_ID][HOH][Mailbox_Index].Check = Confirmed;
             Recieve_Count++; /* increment the receive count */
         }
         MSG_Object_INT_Flag =1; /* raise the message object flag to detect that an interrupt occurred */
@@ -89,60 +125,60 @@ void CAN0_Handler(void)
 
 void CAN1_Handler(void)
 {
-//    MSG_Number_INT =0;  /* clearing the message number variable for re-use */
-//    MSG_Object_INT_Flag=0;  /* clearing the message object flag for re-use */
-//    uint32 status = REG_VAL(CAN1_BASE,CAN_INT_OFFSET);  /* reading the CAN_INT register to detect the interrupt cause */
-//    if (CAN_INT_Status_Interrupt == status )  /* if interrupt is a Status interrupt */
-//    {
-//        Error_Status = REG_VAL(CAN1_BASE,CAN_STS_OFFSET);  /* store the status register in a variable */
-//        REG_VAL(CAN1_BASE,CAN_STS_OFFSET) = ~(CAN_STS_RXOK | CAN_STS_TXOK | CAN_STS_LEC_M);  /* clear the RXOK,TXOK,LEC bits in status register */
-//        Error_Flag =1 ;  /* raise the error flag to detect that an error has occurred */
-//    }
-//    /* interrupt is a Message Object interrupt */
-//    else
-//    {
-//        MSG_Number_INT = status; /* store the message number that caused the interrupt in a variable */
-//        uint8 HOH=ZERO,iter=Can_Configuration.CanConfigSet.CanHardwareObject[HOH].CanHardwareObjectCount;
-//        while( MSG_Number_INT > iter )
-//        {
-//            HOH++;
-//            iter+=Can_Configuration.CanConfigSet.CanHardwareObject[HOH].CanHardwareObjectCount;
-//        }
-//
-//        /* Wait for busy bit to clear */
-//        while(BIT_IS_SET(REG_VAL(CAN1_BASE, CAN_IF1CRQ_OFFSET),BUSY_BIT))
-//        {
-//            /*Do Nothing*/
-//        }
-//        /*
-//         * Only change the interrupt pending state by setting only the
-//         * CAN_IF1CMSK_CLRINTPND bit.
-//         */
-//        REG_VAL(CAN1_BASE, CAN_IF1CMSK_OFFSET) = 0x00000008;
-//        /*
-//         * Send the clear pending interrupt command to the CAN controller.
-//         */
-//        REG_VAL(CAN1_BASE, CAN_IF1CRQ_OFFSET) = MSG_Number_INT & SIX_BIT_MASK;
-//        /* Wait for busy bit to clear */
-//        while(BIT_IS_SET(REG_VAL(CAN1_BASE, CAN_IF1CRQ_OFFSET),BUSY_BIT))
-//        {
-//            /*Do Nothing*/
-//        }
-//
-//        if(Can_Configuration.CanConfigSet.CanHardwareObject[HOH].CanObjectType==TRANSMIT) /* interrupt caused by transmit message object */
-//        {
-//            Object_Check[CAN1_CONTROLLER_ID][HOH][MSG_Number_INT-ONE].Check = Confirmed;
-//            /*CanIf_TxConfirmation()*/
-//            Transmit_Count++; /* increment the transmit count */
-//        }
-//        else if(Can_Configuration.CanConfigSet.CanHardwareObject[HOH].CanObjectType==RECIEVE) /* interrupt caused by receive message object */
-//        {
-//            /*CanIf_Rxindication()*/
-//            Recieve_Count++; /* increment the receive count */
-//        }
-//        MSG_Object_INT_Flag =1; /* raise the message object flag to detect that an interrupt occurred */
-//        Error_Flag =0;  /* nullify the error flag after successful transmission or reception of message */
-//    }
+    //    MSG_Number_INT =0;  /* clearing the message number variable for re-use */
+    //    MSG_Object_INT_Flag=0;  /* clearing the message object flag for re-use */
+    //    uint32 status = REG_VAL(CAN1_BASE,CAN_INT_OFFSET);  /* reading the CAN_INT register to detect the interrupt cause */
+    //    if (CAN_INT_Status_Interrupt == status )  /* if interrupt is a Status interrupt */
+    //    {
+    //        Error_Status = REG_VAL(CAN1_BASE,CAN_STS_OFFSET);  /* store the status register in a variable */
+    //        REG_VAL(CAN1_BASE,CAN_STS_OFFSET) = ~(CAN_STS_RXOK | CAN_STS_TXOK | CAN_STS_LEC_M);  /* clear the RXOK,TXOK,LEC bits in status register */
+    //        Error_Flag =1 ;  /* raise the error flag to detect that an error has occurred */
+    //    }
+    //    /* interrupt is a Message Object interrupt */
+    //    else
+    //    {
+    //        MSG_Number_INT = status; /* store the message number that caused the interrupt in a variable */
+    //        uint8 HOH=ZERO,iter=Can_Configuration.CanConfigSet.CanHardwareObject[HOH].CanHardwareObjectCount;
+    //        while( MSG_Number_INT > iter )
+    //        {
+    //            HOH++;
+    //            iter+=Can_Configuration.CanConfigSet.CanHardwareObject[HOH].CanHardwareObjectCount;
+    //        }
+    //
+    //        /* Wait for busy bit to clear */
+    //        while(BIT_IS_SET(REG_VAL(CAN1_BASE, CAN_IF1CRQ_OFFSET),BUSY_BIT))
+    //        {
+    //            /*Do Nothing*/
+    //        }
+    //        /*
+    //         * Only change the interrupt pending state by setting only the
+    //         * CAN_IF1CMSK_CLRINTPND bit.
+    //         */
+    //        REG_VAL(CAN1_BASE, CAN_IF1CMSK_OFFSET) = 0x00000008;
+    //        /*
+    //         * Send the clear pending interrupt command to the CAN controller.
+    //         */
+    //        REG_VAL(CAN1_BASE, CAN_IF1CRQ_OFFSET) = MSG_Number_INT & SIX_BIT_MASK;
+    //        /* Wait for busy bit to clear */
+    //        while(BIT_IS_SET(REG_VAL(CAN1_BASE, CAN_IF1CRQ_OFFSET),BUSY_BIT))
+    //        {
+    //            /*Do Nothing*/
+    //        }
+    //
+    //        if(Can_Configuration.CanConfigSet.CanHardwareObject[HOH].CanObjectType==TRANSMIT) /* interrupt caused by transmit message object */
+    //        {
+    //            Object_Check[CAN1_CONTROLLER_ID][HOH][MSG_Number_INT-ONE].Check = Confirmed;
+    //            /*CanIf_TxConfirmation()*/
+    //            Transmit_Count++; /* increment the transmit count */
+    //        }
+    //        else if(Can_Configuration.CanConfigSet.CanHardwareObject[HOH].CanObjectType==RECIEVE) /* interrupt caused by receive message object */
+    //        {
+    //            /*CanIf_Rxindication()*/
+    //            Recieve_Count++; /* increment the receive count */
+    //        }
+    //        MSG_Object_INT_Flag =1; /* raise the message object flag to detect that an interrupt occurred */
+    //        Error_Flag =0;  /* nullify the error flag after successful transmission or reception of message */
+    //    }
 }
 
 
