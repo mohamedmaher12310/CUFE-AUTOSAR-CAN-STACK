1aaa5bfe0e09ec8bb0a93d2063e7a322aac2d2f5
AhmedAbdelhamidShokry <asag99@gmail.com>
2023-03-26 22:55:32 +0200

    started implementing PduR

---
+++ PduR.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+ *
+ * Module: PduR
+ *
+ * File Name: PduR.c
+ *
+ * Description: Source file for PduR Module.
+ *
+ * Author: CUFE 2023 Team
+ ******************************************************************************/
+ 
+ /* Include the Det header file*/
+ #include "Det.h"
+ 
+ /*Include the module header file*/
+#include "PduR.h"
+
+ 
---
+++ PduR_Types.h
@@ -0,0 +1,21 @@
+/******************************************************************************
+ *
+ * Module: PduR
+ *
+ * File Name: PduR_Types.h
+ *
+ * Description: PduR type definitions
+ *
+ * Author: CUFE 2023 Team
+ *
+ *******************************************************************************/
+#ifndef PDUR_TYPES_H_
+#define PDUR_TYPES_H_
+
+
+
+
+
+
+
+#endif /* PDUR_TYPES_H_ */
