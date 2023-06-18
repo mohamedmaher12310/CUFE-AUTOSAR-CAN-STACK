8a6afa89d228ffe53cd94634d2ed519fcda07051
mahoora <mohamedmaher12310@gmail.com>
2023-06-19 00:41:57 +0300

    handling

---
+++ Com_Cbk.c
@@ -0,0 +1,81 @@
+/*
+ * Com_Cbk.c
+ *
+ *  Created on: 18 Jun. 2023
+ *      Author: owner
+ */
+
+
+void Com_CbkRxAck_0(void){
+
+}
+void Com_CbkRxAck_1(void){
+
+}
+void Com_CbkRxAck_2(void){
+
+}
+void Com_CbkRxAck_3(void){
+
+}
+void Com_CbkRxAck_4(void){
+
+}
+void Com_CbkRxAck_5(void){
+
+}
+void Com_CbkRxAck_6(void){
+
+}
+void Com_CbkRxAck_7(void){
+
+}
+void Com_CbkRxAck_8(void){
+
+}
+void Com_CbkRxAck_9(void){
+
+}
+void Com_CbkRxAck_10(void){
+
+}
+void Com_CbkRxAck_11(void){
+
+}
+void Com_CbkRxAck_12(void){
+
+}
+void Com_CbkRxAck_13(void){
+
+}
+void Com_CbkRxAck_14(void){
+
+}
+void Com_CbkRxAck_15(void){
+
+}
+void Com_CbkRxAck_16(void){
+
+}
+void Com_CbkRxAck_17(void){
+
+}
+void Com_CbkRxAck_18(void){
+
+}
+void Com_CbkRxAck_19(void){
+
+}
+void Com_CbkRxAck_20(void){
+
+}
+void Com_CbkRxAck_21(void){
+
+}
+void Com_CbkRxAck_22(void){
+
+}
+void Com_CbkRxAck_23(void){
+
+}
+
---
+++ Com_Cbk.h
@@ -0,0 +1,51 @@
+/*
+ * Com_Cbk.h
+ *
+ *  Created on: 18 Jun. 2023
+ *      Author: owner
+ */
+
+#ifndef CUFE_AUTOSAR_CAN_STACK_COM_CBK_H_
+#define CUFE_AUTOSAR_CAN_STACK_COM_CBK_H_
+
+/************************************************************************************
+ * Service Name: Com_CbkRxAck
+ * Sync/Async: Synchronous
+ * Reentrancy: don't care
+ * Parameters (in): None
+ * Parameters (inout): None
+ * Parameters (out): None
+ * Return value: None
+ * Description: This callback represents notification class 1 of [17]. It is called immediately after
+ *              the message has been stored in the receiving message object.
+ *
+ ************************************************************************************/
+
+void Com_CbkRxAck_0(void);
+void Com_CbkRxAck_1(void);
+void Com_CbkRxAck_2(void);
+void Com_CbkRxAck_3(void);
+void Com_CbkRxAck_4(void);
+void Com_CbkRxAck_5(void);
+void Com_CbkRxAck_6(void);
+void Com_CbkRxAck_7(void);
+void Com_CbkRxAck_8(void);
+void Com_CbkRxAck_9(void);
+void Com_CbkRxAck_10(void);
+void Com_CbkRxAck_11(void);
+void Com_CbkRxAck_12(void);
+void Com_CbkRxAck_13(void);
+void Com_CbkRxAck_14(void);
+void Com_CbkRxAck_15(void);
+void Com_CbkRxAck_16(void);
+void Com_CbkRxAck_17(void);
+void Com_CbkRxAck_18(void);
+void Com_CbkRxAck_19(void);
+void Com_CbkRxAck_20(void);
+void Com_CbkRxAck_21(void);
+void Com_CbkRxAck_22(void);
+void Com_CbkRxAck_23(void);
+
+
+
+#endif /* CUFE_AUTOSAR_CAN_STACK_COM_CBK_H_ */
