53fa8f22a0992bfdbc0f971e7cd76048c2e6f032
AhmedAbdelhamidShokry <asag99@gmail.com>
2023-07-05 21:00:24 +0300

    os testing

---
+++ Application/App.h
@@ -0,0 +1,19 @@
+/*
+ * App.h
+ *
+ *  Created on: Jul 5, 2023
+ *      Author: Mally
+ */
+
+#ifndef APPLICATION_APP_H_
+#define APPLICATION_APP_H_
+
+#include "../BSW/Service Layer/Com/Com.h"
+#include "../BSW/MCAL/Port/Port.h"
+#include "../BSW/MCAL/Uart/UART.h"
+
+
+void Init_Task(void);
+
+
+#endif /* APPLICATION_APP_H_ */
